c1c53b6952e212888ea1b66c4866a6264cec5046
darasjohnson <darasjohnson@outlook.com>
2017-10-02 14:32:16 -0400

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ import java.util.*;
 import java.text.*;
 import java.math.*;
 
-public class first {
+public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		
